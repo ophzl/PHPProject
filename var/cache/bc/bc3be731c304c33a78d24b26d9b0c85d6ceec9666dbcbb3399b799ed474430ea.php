121ef686fe41850fbc04e5970f0a2dff29cce2c9
Florian Leroux <florian.leroux3@laposte.net>
2020-02-03 19:01:21 +0100

    Patch User

---
+++ var/cache/bc/bc3be731c304c33a78d24b26d9b0c85d6ceec9666dbcbb3399b799ed474430ea.php
@@ -0,0 +1,300 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* index.html.twig */
+class __TwigTemplate_3cdfc1a19307d1ac08547958dcbd18b32c2bc42d1d9e60324f7f338a763488f3 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+            'css' => [$this, 'block_css'],
+            'body' => [$this, 'block_body'],
+            'javascript' => [$this, 'block_javascript'],
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<!doctype html>
+<html lang=\"fr\">
+<head>
+    <meta charset=\"utf-8\">
+    <title>";
+        // line 5
+        $this->displayBlock('title', $context, $blocks);
+        echo "</title>
+    <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\">
+    <link rel=\"stylesheet\"
+          href=\"https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/pepper-grinder/jquery-ui.css\">
+    <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.8.1/css/all.css\">
+    <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/css/select2.min.css\">
+    ";
+        // line 11
+        $this->displayBlock('css', $context, $blocks);
+        // line 12
+        echo "
+<body>
+
+<nav class=\"navbar sticky-top nav-pills navbar-expand-lg navbar-dark bg-dark\">
+
+    <a class=\"navbar-brand\" href=\"/Article/ListAll\">Blog du CESI</a>
+    <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarTogglerDemo02\"
+            aria-controls=\"navbarTogglerDemo02\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+        <span class=\"navbar-toggler-icon\"></span>
+    </button>
+    <div class=\"collapse navbar-collapse\" id=\"navbarTogglerDemo02\">
+        <!--<div class=\"row\">-->
+
+        <ul class=\"navbar-nav mr-auto mt-2 mt-lg-0\">
+            <li class=\"nav-item\">
+                <a class=\"nav-link\" href=\"/Article/ListAll\">Liste des articles</a>
+            </li>
+            <li class=\"nav-item\">
+                <a class=\"nav-link\" href=\"/Article/Add\">Ajout d'un article</a>
+            </li>
+
+            <li class=\"nav-item dropdown\">
+                <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\"
+                   aria-haspopup=\"true\" aria-expanded=\"false\">
+                    Compte
+                </a>
+                <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">
+                    ";
+        // line 39
+        if (((isset($context["userConnected"]) || array_key_exists("userConnected", $context)) && 0 !== twig_compare(($context["userConnected"] ?? null), null))) {
+            // line 40
+            echo "                        <a class=\"dropdown-item\" href=\"#\">Approuver un article</a>
+                        <a class=\"dropdown-item\" href=\"#\">Gestion des membres</a>
+                        <a class=\"dropdown-item\" href=\"/Logout\">Se déconnecter</a>
+
+                    ";
+        } else {
+            // line 45
+            echo "                        <a class=\"dropdown-item\" href=\"/Login\">Se connecter</a>
+                        <a class=\"dropdown-item\" href=\"#\">S'inscrire</a>
+                    ";
+        }
+        // line 48
+        echo "                </div>
+            </li>
+        </ul>
+
+
+        <form class=\"form-inline\" method=\"post\" action=\"/Article/Show/";
+        // line 53
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["post"] ?? null), "search", [], "any", false, false, false, 53), "html", null, true);
+        echo "\">
+            <input class=\"form-control mr-sm-2\" type=\"search\" placeholder=\"Rechercher un article\" name=\"search\">
+            <input type=\"submit\" class=\"btn btn-outline-success my-2 my-sm-0\" value=\"Rechercher\" name=\"searchSubmit\">
+        </form>
+    </div>
+
+</nav>
+<!--
+<nav class=\"navbar navbar-expand-lg navbar-light bg-light\">
+    <a class=\"navbar-brand\" href=\"#\">Menu</a>
+    <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+        <span class=\"navbar-toggler-icon\"></span>
+    </button>
+
+    <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">
+        <ul class=\"navbar-nav mr-auto\">
+            <li class=\"nav-item dropdown\">
+                <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                    Admin
+                </a>
+                <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">
+                    <a class=\"dropdown-item\" href=\"/?controller=Article&action=ListAll\">Liste</a>
+                    <a class=\"dropdown-item\" href=\"/?controller=Article&action=Add\">Ajout</a>
+                    <div class=\"dropdown-divider\"></div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>-->
+
+
+";
+        // line 84
+        $this->displayBlock('body', $context, $blocks);
+        // line 85
+        echo "
+
+<script src=\"https://code.jquery.com/jquery-3.4.0.min.js\"></script>
+<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\"></script>
+<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\"></script>
+<script src=\"https://code.jquery.com/ui/1.12.1/jquery-ui.js\"></script>
+<script src=\"https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/i18n/jquery-ui-i18n.min.js\"></script>
+<script src=\"https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/js/select2.min.js\"></script>
+";
+        // line 93
+        $this->displayBlock('javascript', $context, $blocks);
+        // line 94
+        echo "</body>
+</html>
+";
+    }
+
+    // line 5
+    public function block_title($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        echo "CESI BLOG";
+    }
+
+    // line 11
+    public function block_css($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+    }
+
+    // line 84
+    public function block_body($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+    }
+
+    // line 93
+    public function block_javascript($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+    }
+
+    public function getTemplateName()
+    {
+        return "index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  181 => 93,  175 => 84,  169 => 11,  162 => 5,  156 => 94,  154 => 93,  144 => 85,  142 => 84,  108 => 53,  101 => 48,  96 => 45,  89 => 40,  87 => 39,  58 => 12,  56 => 11,  47 => 5,  41 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<!doctype html>
+<html lang=\"fr\">
+<head>
+    <meta charset=\"utf-8\">
+    <title>{% block title %}CESI BLOG{% endblock %}</title>
+    <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\">
+    <link rel=\"stylesheet\"
+          href=\"https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/pepper-grinder/jquery-ui.css\">
+    <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.8.1/css/all.css\">
+    <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/css/select2.min.css\">
+    {% block css %}{% endblock %}
+
+<body>
+
+<nav class=\"navbar sticky-top nav-pills navbar-expand-lg navbar-dark bg-dark\">
+
+    <a class=\"navbar-brand\" href=\"/Article/ListAll\">Blog du CESI</a>
+    <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarTogglerDemo02\"
+            aria-controls=\"navbarTogglerDemo02\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+        <span class=\"navbar-toggler-icon\"></span>
+    </button>
+    <div class=\"collapse navbar-collapse\" id=\"navbarTogglerDemo02\">
+        <!--<div class=\"row\">-->
+
+        <ul class=\"navbar-nav mr-auto mt-2 mt-lg-0\">
+            <li class=\"nav-item\">
+                <a class=\"nav-link\" href=\"/Article/ListAll\">Liste des articles</a>
+            </li>
+            <li class=\"nav-item\">
+                <a class=\"nav-link\" href=\"/Article/Add\">Ajout d'un article</a>
+            </li>
+
+            <li class=\"nav-item dropdown\">
+                <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\"
+                   aria-haspopup=\"true\" aria-expanded=\"false\">
+                    Compte
+                </a>
+                <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">
+                    {% if userConnected is defined and userConnected != null %}
+                        <a class=\"dropdown-item\" href=\"#\">Approuver un article</a>
+                        <a class=\"dropdown-item\" href=\"#\">Gestion des membres</a>
+                        <a class=\"dropdown-item\" href=\"/Logout\">Se déconnecter</a>
+
+                    {% else %}
+                        <a class=\"dropdown-item\" href=\"/Login\">Se connecter</a>
+                        <a class=\"dropdown-item\" href=\"#\">S'inscrire</a>
+                    {% endif %}
+                </div>
+            </li>
+        </ul>
+
+
+        <form class=\"form-inline\" method=\"post\" action=\"/Article/Show/{{ post.search }}\">
+            <input class=\"form-control mr-sm-2\" type=\"search\" placeholder=\"Rechercher un article\" name=\"search\">
+            <input type=\"submit\" class=\"btn btn-outline-success my-2 my-sm-0\" value=\"Rechercher\" name=\"searchSubmit\">
+        </form>
+    </div>
+
+</nav>
+<!--
+<nav class=\"navbar navbar-expand-lg navbar-light bg-light\">
+    <a class=\"navbar-brand\" href=\"#\">Menu</a>
+    <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+        <span class=\"navbar-toggler-icon\"></span>
+    </button>
+
+    <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">
+        <ul class=\"navbar-nav mr-auto\">
+            <li class=\"nav-item dropdown\">
+                <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                    Admin
+                </a>
+                <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">
+                    <a class=\"dropdown-item\" href=\"/?controller=Article&action=ListAll\">Liste</a>
+                    <a class=\"dropdown-item\" href=\"/?controller=Article&action=Add\">Ajout</a>
+                    <div class=\"dropdown-divider\"></div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>-->
+
+
+{% block body %}{% endblock %}
+
+
+<script src=\"https://code.jquery.com/jquery-3.4.0.min.js\"></script>
+<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\"></script>
+<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\"></script>
+<script src=\"https://code.jquery.com/ui/1.12.1/jquery-ui.js\"></script>
+<script src=\"https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/i18n/jquery-ui-i18n.min.js\"></script>
+<script src=\"https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/js/select2.min.js\"></script>
+{% block javascript %}{% endblock %}
+</body>
+</html>
+", "index.html.twig", "C:\\Users\\tetra\\Documents\\DEV\\PHP\\projetCESI\\templates\\index.html.twig");
+    }
+}
